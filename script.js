221267be63db2a99f856c3954999b70bb685a7e2
Priyanka Maity <74773327+Priyanka-Maity@users.noreply.github.com>
2024-07-24 16:25:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    document.querySelectorAll('.scroll-animation').forEach(el => {
+        observer.observe(el);
+    });
+});
